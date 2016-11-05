4fd12c5cc66be46f8f149bd6eed97e9ab8be5abb
ruitaoH <896785634@qq.com>
2016-11-05 15:01:04 +0800

    update time format

--- backend/util.py
+++ backend/util.py
@@ -1,8 +1,12 @@
 import random
+import time
 
 # 格式化转换datetime
 def getStrTime(datetime):
     return datetime.strftime('%Y-%m-%d %H:%M:%S')
+
+def timestamp2Str(timestamp):
+    return time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(timestamp))
 
 def getMeetDistance():
     return random.randint(5,100)
