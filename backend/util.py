4f292a0d16e3ab4258b38d19b27117acf71bbac3
ruitaoH <896785634@qq.com>
2016-11-09 20:28:53 +0800

    test json

--- backend/util.py
+++ backend/util.py
@@ -1,12 +1,13 @@
 import random
 import time
+from datetime import datetime
 
 # 格式化转换datetime ---> 精确到毫秒
 def getStrTime(datetime):
     return datetime.strftime('%Y-%m-%d %H:%M:%S %f')
 
 def timestamp2Str(timestamp):
-    return time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(timestamp)) + ' ' + str(int((timestamp * 1000000) % 1000000))
+    return getStrTime(datetime.fromtimestamp(timestamp))
 
 def getMeetDistance():
     return random.randint(5,100)
